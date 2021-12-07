76077241cc7b1dfca587d718ad54a1faea9401ad
Luka2477 <luke.da.duke@hotmail.com>
2021-12-07 21:38:15 +0100

    Did some stuff

--- RainTyper/src/application/model/Difficulty.java
+++ RainTyper/src/application/model/Difficulty.java
@@ -1,5 +1,21 @@
 package application.model;
 
 public enum Difficulty {
-    EASY, MEDIUM, HARD
+    EASY(1, 4), MEDIUM(5, 7), HARD(8, 22);
+
+    private int minLengthWord;
+    private int maxLengthWord;
+
+    Difficulty (int minLengthWord, int maxLengthWord) {
+        this.minLengthWord = minLengthWord;
+        this.maxLengthWord = maxLengthWord;
+    }
+
+    public int getMinLengthWord() {
+        return this.minLengthWord;
+    }
+
+    public int getMaxLengthWord () {
+        return this.maxLengthWord;
+    }
 }
--- RainTyper/src/gui/GameWindow.java
+++ RainTyper/src/gui/GameWindow.java
@@ -7,10 +7,20 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
 public class GameWindow extends Stage {
 
@@ -22,10 +32,19 @@ public class GameWindow extends Stage {
     private TextField txfWord;
     private Label lblWordsPerMinute, lblAccuracy, lblPasses, lblMisses;
 
+    private ArrayList<String> wordsList = new ArrayList<>();
+    private ArrayList<String> words = new ArrayList<>();
+
+    private int missed = 0, passed = 0, backspaces = 0, correctChars = 0;
+
+    private LocalTime startTime;
+
     public GameWindow(Difficulty difficulty, int duration, int width) {
         this.difficulty = difficulty;
         this.duration = duration;
         this.width = width;
+
+        this.startTime = LocalTime.now();
 
         this.setTitle("Rain Typer");
         GridPane pane = new GridPane();
@@ -42,13 +61,14 @@ public class GameWindow extends Stage {
         pane.setGridLinesVisible(true);
 
         this.wordsPane = new Pane();
-        this.wordsPane.setPrefWidth(this.width * (this.difficulty == Difficulty.EASY ? 40 : (this.difficulty == Difficulty.MEDIUM ? 70 : 150)));
+        this.wordsPane.setPrefWidth(this.width * this.difficulty.getMaxLengthWord() * 10);
         this.wordsPane.setPrefHeight(600);
         this.wordsPane.setBackground(new Background(new BackgroundFill(Color.BLACK, new CornerRadii(0), new Insets(0))));
         pane.add(this.wordsPane, 0, 0);
 
         this.txfWord = new TextField();
-        this.txfWord.setPrefWidth(150);
+        this.txfWord.setPrefWidth(Difficulty.HARD.getMaxLengthWord() * 7);
+        this.txfWord.setOnKeyPressed(event -> handleTxfKeyPressed(event.getCode()));
         this.lblWordsPerMinute = new Label("WPM: 0");
         this.lblAccuracy = new Label("Acc: 0.0%");
         this.lblPasses = new Label("Pass: 0");
@@ -62,7 +82,62 @@ public class GameWindow extends Stage {
         this.initGame();
     }
 
+    private void handleTxfKeyPressed (KeyCode key) {
+        if (key == KeyCode.BACK_SPACE) {
+            this.backspaces++;
+
+            this.updateStats();
+        }else if (key == KeyCode.SPACE || key == KeyCode.ENTER) {
+            String input = this.txfWord.getText().trim();
+            boolean passed = this.words.remove(input);
+
+            if (passed) {
+                this.correctChars += input.length();
+                this.passed++;
+            } else {
+                this.missed++;
+            }
+
+            this.words.clear();
+            this.updateStats();
+        }
+    }
+
+    private void updateStats () {
+        double acc = (double) this.backspaces / this.correctChars * 100;
+        double wpm = (double) this.passed / ChronoUnit.SECONDS.between(this.startTime, LocalTime.now()) / 60;
+
+        this.lblMisses.setText("Miss: " + this.missed);
+        this.lblPasses.setText("Pass: " + this.passed);
+        this.lblAccuracy.setText(String.format("Acc: %3.2f%%", acc != 0 ? acc : 100.0));
+        this.lblWordsPerMinute.setText(String.format("WPM: %.1f", wpm));
+    }
+
+    private void initWords () {
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("raintyper/src/storage/words.json")) {
+            Object obj = jsonParser.parse(reader);
+
+            JSONArray words = (JSONArray) obj;
+
+            for (Object word : words) {
+                String str = word.toString();
+
+                if (str.length() >= this.difficulty.getMinLengthWord() && str.length() <= this.difficulty.getMaxLengthWord()) {
+                    this.wordsList.add(str);
+                }
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void initGame () {
+        this.initWords();
+    }
+
+    private void loop () {
 
     }
 }
