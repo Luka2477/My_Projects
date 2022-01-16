d43cc5138aafa3a2e4911ece7be7a9f6ecba6c3a
Luka2477 <luke.da.duke@hotmail.com>
2022-01-16 14:16:49 +0100

    Added input, timeline, generation of points and animation of timeline

---
+++ ApproximatingPI/src/gui/CountContext.java
@@ -0,0 +1,22 @@
+package gui;
+
+public class CountContext {
+
+    private double x, y;
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void addX(double x) {
+        this.x += x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void addY(double y) {
+        this.y += y;
+    }
+}
--- ApproximatingPI/src/gui/GUI.java
+++ ApproximatingPI/src/gui/GUI.java
@@ -1,10 +1,11 @@
 package gui;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -13,6 +14,9 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.Scanner;
 
 public class GUI extends Application {
 
@@ -29,33 +33,40 @@ public class GUI extends Application {
 
         this.initContent(pane);
 
-        // TODO Ask for input
-
         Scene scene = new Scene(pane);
         stage.setScene(scene);
         stage.show();
     }
 
     private void initContent (Pane pane) {
-        Point2D boundstl = new Point2D(this.width / 12, this.height / 24 + this.height / 12);
-        Point2D boundsbr = new Point2D(this.width / 12 * 11, this.height / 24 * 23);
+        Point2D boundCoords = new Point2D(this.width / 12, this.height / 24 + this.height / 12);
+        Point2D boundSize = new Point2D(this.width / 12 * 10, this.height / 24 * 20);
         Point2D center = new Point2D(this.width / 2, this.height / 24 + this.height / 2);
         double radius = this.width / 12 * 5;
-        double pointRadius = this.width / 250;
-        double pi = 0.0;
+        double pointRadius = this.width / 500;
+        CountContext count = new CountContext();
         Group group = new Group();
         pane.getChildren().add(group);
+
+        // Ask for input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many points (int): ");
+        int points = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("How many milliseconds delay (double): ");
+        double delay = Double.parseDouble(scanner.nextLine());
 
         // Create background
         Rectangle background = new Rectangle(0, 0, this.width, this.height);
         group.getChildren().add(background);
 
         // Create bounding box
-        Rectangle bounds = new Rectangle(boundstl.getX(), boundstl.getY(), boundsbr.getX() - boundstl.getX(), boundsbr.getY() - boundstl.getY());
-        bounds.setFill(Color.TRANSPARENT);
-        bounds.setStroke(Color.WHITE);
-        bounds.setStrokeWidth(pointRadius);
-        group.getChildren().add(bounds);
+        Rectangle bound = new Rectangle(boundCoords.getX(), boundCoords.getY(), boundSize.getX(), boundSize.getY());
+        bound.setFill(Color.TRANSPARENT);
+        bound.setStroke(Color.WHITE);
+        bound.setStrokeWidth(pointRadius);
+        group.getChildren().add(bound);
 
         // Create circle that we will use to approximate PI
         Circle circle = new Circle(center.getX(), center.getY(), radius);
@@ -65,7 +76,7 @@ public class GUI extends Application {
         group.getChildren().add(circle);
 
         // Text to show approximation
-        Text approximation = new Text(String.format("PI = %f", pi));
+        Text approximation = new Text();
         approximation.setTextAlignment(TextAlignment.CENTER);
         approximation.setX(this.width / 12);
         approximation.setY(this.height / 12);
@@ -73,12 +84,34 @@ public class GUI extends Application {
         approximation.setFill(Color.WHITE);
         group.getChildren().add(approximation);
 
-        // TODO Create Timeline
+        // Create Timeline
+        Timeline timeline = new Timeline();
 
-        // TODO Generate points in Keyframes
+        // Generate points in Keyframes
+        for (int i = 0; i < points; i++) {
+            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(i * delay), event -> {
+                Point2D pointCoords = new Point2D(
+                        boundCoords.getX() + Math.random() * boundSize.getX(),
+                        boundCoords.getY() + Math.random() * boundSize.getY());
+                Circle point = new Circle(pointCoords.getX(), pointCoords.getY(), pointRadius);
 
-        // TODO Animate the timeline
+                if (center.distance(pointCoords.getX(), pointCoords.getY()) <= radius) {
+                    point.setFill(Color.DARKRED);
+                    count.addX(1);
+                } else {
+                    point.setFill(Color.DARKBLUE);
+                }
+                count.addY(1);
 
+                group.getChildren().add(point);
+
+                approximation.setText(String.format("PI is approximately %.15f", 4 * count.getX() / count.getY()));
+            }));
+        }
+
+        // Animate the timeline
+        timeline.setCycleCount(1);
+        timeline.play();
     }
 
 }
